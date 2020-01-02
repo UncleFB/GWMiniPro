7254843e5b3b515ca0b5bdceb6b22763ae93a996
fubo <371141000@fubo.com>
2020-01-02 17:23:28 +0800

    first commit

---
+++ pages/list/list.js
@@ -0,0 +1,36 @@
+// pages/list/list.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    listArr: [
+      {
+        mac: 123
+      }, {
+        mac: 456
+      }, {
+        mac: 789
+      }
+    ]
+    
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    toDetail(){
+      wx.navigateTo({
+        url: '/pages/detail/detail',
+      })
+    }
+  }
+})
